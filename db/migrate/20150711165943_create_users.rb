0f08a5c1be2bcf0c4cdca98fa20e93f894b4fc65
Shota Hirao <shota.hirao@gmail.com>
2015-07-12 02:06:54 +0900

    usersテーブルの作成

--- db/migrate/20150711165943_create_users.rb
+++ db/migrate/20150711165943_create_users.rb
@@ -1,8 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :name
+      t.string :fb_id
       t.timestamps null: false
     end
+
+    add_index :users, :name
+    add_index :users, :fb_id
   end
 end
