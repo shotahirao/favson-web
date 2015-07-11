5d518f1d896bbf1327ee49523e6338239f62dba3
Shota Hirao <shota.hirao@gmail.com>
2015-07-12 03:54:42 +0900

    ログインの遷移部分の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
 
+  def index
+  end
+
   def login
   end
+
+  def create
+    # Facebookログイン
+    user  = User.from_omniauth(env['omniauth.auth'])
+    # ログイン後のページへ
+    redirect_to :root and return if user.save
+    # ログイン失敗
+    redirect_to auth_failure_path
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+
+  def self.from_omniauth(auth)
+    user = User.new
+    user.fb_id   = auth.uid
+    user.name  = auth.info.name
+    user.email = auth.info.email
+    user.icon  = auth.info.image
+    user.oauth_token      = auth.credentials.token
+    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+
+    user
+  end
+
 end
